524343f2cadcfcc4c29abb428fb003a74dc48a4f
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-03-12 08:40:39 -0300

    Refactor webpack.config.js code style

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 var path = require('path');
 
 module.exports = {
-    entry: path.resolve(path.join(__dirname, '.', 'index.js')),
-    output: {
-        path: path.resolve(path.join(__dirname, '.', 'dist')),
-        library: 'ReactClipboard',
-        libraryTarget: 'umd',
-        filename: 'react-clipboard.js',
-        globalObject: 'this',
-    },
-    module: {
-        rules: [{
-            test: /\.js?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader'
-        }]
-    },
-    externals: [
-        {react: {root: 'React', amd: 'react', commonjs: 'react', commonjs2: 'react'}},
-        {'prop-types': {root: 'PropTypes', amd: 'prop-types', commonjs: 'prop-types', commonjs2: 'prop-types'}},
-        {clipboard: {root: 'ClipboardJS', amd: 'clipboard', commonjs: 'clipboard', commonjs2: 'clipboard'}}
-    ]
+  entry: path.resolve(path.join(__dirname, '.', 'index.js')),
+  output: {
+    path: path.resolve(path.join(__dirname, '.', 'dist')),
+    library: 'ReactClipboard',
+    libraryTarget: 'umd',
+    filename: 'react-clipboard.js',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader'
+    }]
+  },
+  externals: [
+    {react: {root: 'React', amd: 'react', commonjs: 'react', commonjs2: 'react'}},
+    {'prop-types': {root: 'PropTypes', amd: 'prop-types', commonjs: 'prop-types', commonjs2: 'prop-types'}},
+    {clipboard: {root: 'ClipboardJS', amd: 'clipboard', commonjs: 'clipboard', commonjs2: 'clipboard'}}
+  ]
 };
